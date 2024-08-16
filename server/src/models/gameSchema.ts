52d34c145f7925711697243723c2ce24531613e8
szymg2001 <szymon.grabowiec2001@gmail.com>
2024-08-16 19:20:41 +0200

    Added game schema

---
+++ server/src/models/gameSchema.ts
@@ -0,0 +1,47 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type GameDocument = HydratedDocument<Game>;
+
+@Schema()
+export class GameCard {
+  @Prop()
+  value: string;
+
+  @Prop()
+  isSpecial: string;
+
+  @Prop()
+  instruction: string;
+}
+
+const GameCardSchema = SchemaFactory.createForClass(GameCard);
+
+@Schema()
+export class GameUser {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  userId: mongoose.Schema.Types.ObjectId;
+
+  @Prop()
+  isOwner: boolean;
+
+  @Prop([GameCardSchema])
+  cardsInHand: GameCard[];
+}
+
+const GameUserSchema = SchemaFactory.createForClass(GameUser);
+
+@Schema()
+export class Game {
+  @Prop()
+  status: 'lobby' | 'started' | 'endScreen';
+
+  @Prop([GameUserSchema])
+  users: GameUser[];
+
+  @Prop([GameCardSchema])
+  drawPile: GameCard[];
+
+  @Prop([GameCardSchema])
+  discardPile: GameCard[];
+}
