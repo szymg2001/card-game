1dfaeba18a1ac99c94bdd83fe8d898bc8400955c
szymg2001 <szymon.grabowiec2001@gmail.com>
2024-09-04 17:12:18 +0200

    Update redux toolkit store - Add listenerMiddleware to look for changes and update object in localstorage - Change initialState in userSlice to check for localstorage data

--- client/src/store/store.tsx
+++ client/src/store/store.tsx
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import listenerMiddleWare from "./listenerMiddleware";
 
 const store = configureStore({
   reducer: { user: userReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleWare.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
