738be79cee6c28a213efd3fb89baefe7274cde60
szymg2001 <szymon.grabowiec2001@gmail.com>
2024-09-04 18:42:29 +0200

    Add store provider to index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+
 import RouterComponent from "./components/Router/RouterComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="app-container">
       <RouterComponent />
